7f2c43cda6118319f836c500f2b92364d89ba879
carey <746040067@qq.com>
2019-12-01 20:30:37 +0800

    test

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName:Test
+ * Package:PACKAGE_NAME
+ * Description:
+ *
+ * @Date:2019/12/1 20:22
+ * @author:carey
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
